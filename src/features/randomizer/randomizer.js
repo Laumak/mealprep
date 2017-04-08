1225c750a136c2eae1f3740dd0c1204b94a0e85f
Lauri Mäkinen <lauri@mits.fi>
2017-04-08 22:16:33 +0300

    Package updates.

--- src/features/randomizer/randomizer.js
+++ src/features/randomizer/randomizer.js
@@ -5,53 +5,53 @@ import LoadingButton from "../../components/loadingButton";
 import Card from "../../components/card";
 
 class Randomizer extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            loading: false,
-            mealVisible: false,
-        };
+    this.state = {
+      loading: false,
+      mealVisible: false,
+    };
 
-        this.generateMeal = this.generateMeal.bind(this);
-    }
+    this.generateMeal = this.generateMeal.bind(this);
+  }
 
-    generateMeal() {
-        this.setState({ loading: true });
+  generateMeal() {
+    this.setState({ loading: true });
 
-        setTimeout(() => {
-            this.setState({
-                mealVisible: true,
-                loading: false,
-            });
-        }, 1000);
-    }
+    setTimeout(() => {
+      this.setState({
+        mealVisible: true,
+        loading: false,
+      });
+    }, 1000);
+  }
 
-    render() {
-        return(
-            <div>
-                <div className="generator" style={{ marginBottom: 20 }}>
-                    <LoadingButton
-                        className="button is-success is-large is-fullwidth"
-                        text="Generate a random meal"
-                        loading={this.state.loading}
-                        handleOnClick={this.generateMeal}
-                    />
-                </div>
+  render() {
+    return(
+      <div>
+        <div className="generator" style={{ marginBottom: 20 }}>
+          <LoadingButton
+            className="button is-success is-large is-fullwidth"
+            text="Generate a random meal"
+            loading={this.state.loading}
+            handleOnClick={this.generateMeal}
+          />
+        </div>
 
-                <Loader loading={this.state.loading}>
-                    {
-                        this.state.mealVisible &&
-                        <Card title="Meal name">
-                            <article className="content">
-                                <p>Meal description</p>
-                            </article>
-                        </Card>
-                    }
-                </Loader>
-            </div>
-        );
-    }
+        <Loader loading={this.state.loading}>
+        {
+          this.state.mealVisible &&
+            <Card title="Meal name">
+              <article className="content">
+                <p>Meal description</p>
+              </article>
+            </Card>
+        }
+        </Loader>
+      </div>
+    );
+  }
 }
 
 export default Randomizer;
