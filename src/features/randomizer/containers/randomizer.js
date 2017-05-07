846b695e61f0d7afc6fbb8b217ed32155f4ce2b6
Lauri Mäkinen <lauri@mits.fi>
2017-05-07 11:57:08 +0300

    Randomizer contentWrapper

--- src/features/randomizer/containers/randomizer.js
+++ src/features/randomizer/containers/randomizer.js
@@ -6,6 +6,8 @@ import navigate from "../../../utils/navigate"
 
 import { FetchRandomMeal } from "../actions"
 
+import Hero from "../../../components/hero"
+import ContentWrapper from "../../../components/contentWrapper"
 import Loader from "../../../components/loader"
 import LoadingButton from "../../../components/loadingButton"
 import Card from "../../../components/card"
@@ -67,58 +69,62 @@ class Randomizer extends Component {
 
     return (
       <div className="randomizer">
-        <div className="generator">
-          <div className="options">
-            <RadioGroup
-              options={options}
-              onChange={this.handleOnRadioChange}
-              selectedValue={this.state.mealType}
-            />
+        <Hero title="Randomizer" />
 
-            {
-              !!this.state.mealType &&
-                <button
-                  className="button is-danger clear-selection"
-                  onClick={() => this.setState({ mealType: null })}
-                >
-                  Clear
-                </button>
-            }
+        <ContentWrapper>
+          <div className="generator">
+            <div className="options">
+              <RadioGroup
+                options={options}
+                onChange={this.handleOnRadioChange}
+                selectedValue={this.state.mealType}
+              />
+
+              {
+                !!this.state.mealType &&
+                  <button
+                    className="button is-danger clear-selection"
+                    onClick={() => this.setState({ mealType: null })}
+                  >
+                    Clear
+                  </button>
+              }
+            </div>
+
+            <LoadingButton
+              className="button is-success is-large is-fullwidth"
+              text="Generate a random meal"
+              loading={this.state.loading}
+              handleOnClick={this.generateMeal}
+            />
           </div>
 
-          <LoadingButton
-            className="button is-success is-large is-fullwidth"
-            text="Generate a random meal"
-            loading={this.state.loading}
-            handleOnClick={this.generateMeal}
-          />
-        </div>
+          <Loader loading={this.state.loading}>
+            <span>
+            {
+              this.state.mealVisible &&
+                <Card
+                  title={randomMeal.title}
+                  id={randomMeal.id}
+                  onHeaderClick={this.onHeaderClick}
+                >
+                  <article className="content">
+                    <p>{randomMeal.description}</p>
+                  </article>
+                </Card>
+            }
 
-        <Loader loading={this.state.loading}>
-          <span>
-          {
-            this.state.mealVisible &&
-              <Card
-                title={randomMeal.title}
-                id={randomMeal.id}
-                onHeaderClick={this.onHeaderClick}
-              >
-                <article className="content">
-                  <p>{randomMeal.description}</p>
-                </article>
-              </Card>
-          }
-
-          {
-            this.state.noResults &&
-              <Card title="No results found">
-                <article className="content">
-                  <p>No results for the given filters.</p>
-                </article>
-              </Card>
-          }
-          </span>
-        </Loader>
+            {
+              this.state.noResults &&
+                <Card title="No results found">
+                  <article className="content">
+                    <p>No results for the given filters.</p>
+                  </article>
+                </Card>
+            }
+            </span>
+          </Loader>
+        </ContentWrapper>
       </div>
     )
   }
